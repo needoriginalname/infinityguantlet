625a2c33ed4cd1149e4a381131bfc0987cf3ab67
Al <needoriginalname@gmail.com>
2016-09-28 16:00:44 -0400

    Fixed minor bug, that didn't return the new position

--- src/main/java/com/needoriginalname/infinitygauntlet/dimension/SpaceGemTeleporter.java
+++ src/main/java/com/needoriginalname/infinitygauntlet/dimension/SpaceGemTeleporter.java
@@ -52,7 +52,7 @@ public class SpaceGemTeleporter extends Teleporter {
             e.setPosition(pos.getX(), pos.getY(), pos.getZ());
 
             MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP) e, worldServerInstance.provider.getDimensionId(), this);
-
+            e.setPosition(pos.getX(), pos.getY(), pos.getZ());
 
         }
     }
@@ -62,65 +62,34 @@ public class SpaceGemTeleporter extends Teleporter {
         double newX = (vec.getX() /world.provider.getMovementFactor());
         double newY = vec.getY() / world.provider.getMovementFactor();
         double newZ = vec.getZ() / world.provider.getMovementFactor();
-        if (entity.dimension != -1){
-            boolean canFindHigherGround = false;
             newY = (float)entity.posY;
             if (newY < 5 || newY >= world.getActualHeight() - 10)
                 newY = 5;
 
             while (true){
                 if (world.getBlockState(new BlockPos(newX, newY, newZ)).getBlock() == Blocks.air || newY >= world.getActualHeight()){
-                    canFindHigherGround = true;
                     break;
                 }
                 newY++;
             }
 
-            if (canFindHigherGround){
-                while (world.getBlockState(new BlockPos(newX, newY-1, newZ)).getBlock() == Blocks.air && newY > 0){
+            while (world.getBlockState(new BlockPos(newX, newY-1, newZ)).getBlock() == Blocks.air && newY > 0){
                     newY--;
-                }
-            }else{
-                if (newY < 5)
-                    newY = 5;
-                if (newY > world.getActualHeight() - 10)
-                    newY = world.getActualHeight() - 10;
-
-                for (int q = (int)Math.floor(newY) - 2; q < newY + 1; ++q){
-                    for (int i = (int)Math.floor(newX) - 1; i < newX + 1; ++i){
-                        for (int k = (int)Math.floor(newZ) - 1; k < newZ + 1; ++k){
-                            if (q == (int)Math.floor(newY - 2)){
-                                world.setBlockState(new BlockPos(i, q, k), Blocks.air.getDefaultState());
-                            }
+            }
+            for (int q = (int)Math.floor(newY) - 2; q < newY + 1; ++q){
+                for (int i = (int)Math.floor(newX) - 1; i < newX + 1; ++i){
+                    for (int k = (int)Math.floor(newZ) - 1; k < newZ + 1; ++k){
+                        if (q == (int)Math.floor(newY - 2)){
+                            world.setBlockState(new BlockPos(i, q, k), Blocks.cobblestone.getDefaultState());
+                        }else{
+                            world.setBlockState(new BlockPos(i, q, k), Blocks.air.getDefaultState());
                         }
                     }
                 }
             }
-        }else{
-            boolean canFindHigherGround = false;
-            while (true){
-                if (world.getBlockState(new BlockPos(newX, newY-1, newZ)).getBlock() == Blocks.air || newY >= 256){
-                    canFindHigherGround = true;
-                    break;
-                }
-                newY++;
-            }
 
-            if (!canFindHigherGround){
-                for (int q = (int)Math.floor(newY) - 2; q < newY + 1; ++q){
-                    for (int i = (int)Math.floor(newX) - 1; i < newX + 1; ++i){
-                        for (int k = (int)Math.floor(newZ) - 1; k < newZ + 1; ++k){
-                            if (q == (int)Math.floor(newY - 2)){
-                                world.setBlockState(new BlockPos(i, q, k), Blocks.netherrack.getDefaultState());
-                            }else{
-                                world.setBlockState(new BlockPos(i, q, k), Blocks.air.getDefaultState());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return vec;
+
+        return new BlockPos(newX, newY, newZ);
     }
 
 
