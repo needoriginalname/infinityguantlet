285d7a7ff215d2cf3ea30e66ee31d8a55e9c9d69
Al <needoriginalname@gmail.com>
2016-10-05 20:05:57 -0400

    hotfix to fix bug teleporting to the End.
    Better solution then using Default teleporter should be used.

--- src/main/java/com/needoriginalname/infinitygauntlet/dimension/SpaceGemTeleporter.java
+++ src/main/java/com/needoriginalname/infinitygauntlet/dimension/SpaceGemTeleporter.java
@@ -44,14 +44,23 @@ public class SpaceGemTeleporter extends Teleporter {
     public void teleport(EntityLivingBase e){
         if (e instanceof EntityPlayerMP && e.worldObj.provider.getDimensionId() != worldServerInstance.provider.getDimensionId()){
             EntityPlayerMP player = (EntityPlayerMP) e;
-            
+
+
+            boolean comingToTheEnd = worldServerInstance.provider.getDimensionId() == 1;
+
+            //a hotfix that prevents the player when traveling to the End on a never loaded chunk.
+            if (comingToTheEnd) {
+                player.travelToDimension(1);
+                return;
+            }
+
             BlockPos pos = clearTeleportPath(worldServerInstance, player);
             boolean comingFromEnd = player.dimension == 1;
+
 
             //stop falling and motion
             player.motionX = player.motionY = player.motionZ = 0.0D;
             player.fallDistance = 0;
-            player.setPosition(pos.getX(), pos.getY(), pos.getZ());
 
             MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension((EntityPlayerMP) e, worldServerInstance.provider.getDimensionId(), this);
 
@@ -59,8 +68,13 @@ public class SpaceGemTeleporter extends Teleporter {
             if (comingFromEnd){
                 e.setLocationAndAngles(pos.getX()+.5d, pos.getY()+.5d, pos.getZ()+.5d, player.rotationYaw, player.rotationPitch);
                 player.worldObj.spawnEntityInWorld(e);
+
                 player.worldObj.updateEntityWithOptionalForce(e, false);
+            } else {
+                player.worldObj.provider.createChunkGenerator().provideChunk(pos);
+                e.setLocationAndAngles(pos.getX()+.5d, pos.getY()+.5d, pos.getZ()+.5d, player.rotationYaw, player.rotationPitch);
             }
+            player.playerNetServerHandler.setPlayerLocation(pos.getX()+.5d, pos.getY()+.5d, pos.getZ()+.5d, player.rotationYaw, player.rotationPitch);
             player.removeExperienceLevel(0);
             player.setPlayerHealthUpdated();
             
@@ -71,29 +85,36 @@ public class SpaceGemTeleporter extends Teleporter {
     private BlockPos clearTeleportPath(World world, EntityLivingBase entity) {
         BlockPos vec = new BlockPos(entity);
         double newX = (vec.getX() /world.provider.getMovementFactor());
-        double newY = vec.getY() / world.provider.getMovementFactor();
+        double newY;
         double newZ = vec.getZ() / world.provider.getMovementFactor();
             newY = (float)entity.posY;
             if (newY < 5 || newY >= world.getActualHeight() - 10)
                 newY = 5;
 
+        //find an air block to teleport toe
             while (true){
-                if (world.getBlockState(new BlockPos(newX, newY, newZ)).getBlock() == Blocks.air || newY >= world.getActualHeight()){
+                if (newY > 5 && (world.getBlockState(new BlockPos(newX, newY, newZ)).getBlock() == Blocks.air || newY >= world.getActualHeight())){
                     break;
                 }
                 newY++;
             }
 
-            while (world.getBlockState(new BlockPos(newX, newY-1, newZ)).getBlock() == Blocks.air && newY > 0){
+            while (world.getBlockState(new BlockPos(newX, newY-1, newZ)).getBlock() == Blocks.air && newY > 5){
                     newY--;
             }
-            for (int q = (int)Math.floor(newY) - 2; q < newY + 1; ++q){
-                for (int i = (int)Math.floor(newX) - 1; i < newX + 1; ++i){
-                    for (int k = (int)Math.floor(newZ) - 1; k < newZ + 1; ++k){
+            for (int q = (int)Math.floor(newY) - 2; q <= newY + 1; ++q){
+                for (int i = (int)Math.floor(newX) - 1; i <= newX + 1; ++i){
+                    for (int k = (int)Math.floor(newZ) - 1; k <= newZ + 1; ++k){
+                        //if block is empty fill it
                         if (q == (int)Math.floor(newY - 2)){
-                            world.setBlockState(new BlockPos(i, q, k), Blocks.cobblestone.getDefaultState());
+                            boolean r = world.setBlockState(new BlockPos(i, q, k), Blocks.cobblestone.getDefaultState());
+                            System.out.println(r);
+                            //assert r;
                         }else{
-                            world.setBlockState(new BlockPos(i, q, k), Blocks.air.getDefaultState());
+                            boolean r2 = world.setBlockState(new BlockPos(i, q, k), Blocks.air.getDefaultState());
+                            System.out.println(r2);
+
+                            //assert r;
                         }
                     }
                 }
